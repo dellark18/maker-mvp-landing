185f244eeeab8166b5ba1fe00ca898fe4ce228f0
Dele <ajayibamidele60@gmail.com>
2023-07-15 11:49:50 +0100

    email validation

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-function myFunction() {
-    alert("I am an alert box!");
+// const email = document.getElementById("email");
+// const form = document.querySelector("form");
+// const errorMessage = document.getElementById("errorMessage");
+
+// form.addEventListener("submit", (e) =>{
+//     const errors = [];
+
+//     if (errors.length > 0){
+//         e.preventDefault();
+//         errorMessage.toggleAttribute("hidden");
+//         errorMessage.innerHTML = errors.join(", ");
+//     }
+// })
+
+function validate() {
+    const email = document.getElementById("email").value;
+    var regEx = /\S+@\S+\.\S+/;
+    if (regEx.test(email)) {
+        document.getElementById("messageStatus").innerHTML = "";
+    } else {
+        document.getElementById("messageStatus").innerHTML = "Oops! That doesn’t look like an email address";
+        messageStatus.toggleAttribute("hidden");
+    }
+
 }
